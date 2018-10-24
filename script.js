485af0c554873fc0665a8f4919b428fc27d507a9
Nova <Johnny@mottl.us>
2018-10-23 20:19:41 -0400

    Finalized

---
+++ script.js
@@ -0,0 +1,18 @@
+var main = function () {
+
+};
+
+$(document).ready(() => {
+
+    $(".more-btn").on("click", () => {
+        $(event.currentTarget).siblings(".more-menu").toggle();
+    })
+
+    $(".share").on("click", () => {
+        $(event.currentTarget).next().toggle();
+    })
+
+    $(".bell").on("click", () => {
+        $(event.currentTarget).find("span").toggleClass("active");
+    })
+});
